f8b0c77266c25e792f0f18a823d3dad1ce19ec32
Yichen LU <scyyl15@nottingham.ac.uk>
2022-04-15 23:12:02 +0100

    create initial

--- src/com/aim/Problem.java
+++ src/com/aim/Problem.java
@@ -1,5 +1,7 @@
 package com.aim;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Problem {
@@ -63,11 +65,59 @@ public class Problem {
 		
 		StringBuffer solutionBuffer = new StringBuffer(solution);
 
-			if (solutionBuffer.charAt(position) == '0') {
-				solutionBuffer.setCharAt(position, '1');
-			}else {
-				solutionBuffer.setCharAt(position, '1');
-			}
-
+		if (solutionBuffer.charAt(position) == '0') {
+			solutionBuffer.setCharAt(position, '1');
+		}else {
+			solutionBuffer.setCharAt(position, '1');
+		}
 	}
+	
+	public List<String> initialSolution(){
+		
+		String solution1 = new String("000000");
+		String solution2 = new String("000000");
+		String solution3 = new String("000000");
+		String solution4 = new String("000000");
+		String solution5 = new String("000000");
+		String solution6 = new String("000000");
+		String solutionChild = new String("000000");
+		
+		Random random = new Random();
+		
+		for (int i = 0; i < random.nextInt(itemNum); i++) {
+			bitFlip(random.nextInt(itemNum),solution1);
+		}
+		for (int i = 0; i < random.nextInt(itemNum); i++) {
+			bitFlip(random.nextInt(itemNum),solution2);
+		}
+		for (int i = 0; i < random.nextInt(itemNum); i++) {
+			bitFlip(random.nextInt(itemNum),solution3);
+		}
+		for (int i = 0; i < random.nextInt(itemNum); i++) {
+			bitFlip(random.nextInt(itemNum),solution4);
+		}
+		for (int i = 0; i < random.nextInt(itemNum); i++) {
+			bitFlip(random.nextInt(itemNum),solution5);
+		}
+		for (int i = 0; i < random.nextInt(itemNum); i++) {
+			bitFlip(random.nextInt(itemNum),solution6);
+		}
+		
+		List<String> solutionList = new ArrayList();
+		solutionList.add(solution1);
+		solutionList.add(solution2);
+		solutionList.add(solution3);
+		solutionList.add(solution4);
+		solutionList.add(solution5);
+		solutionList.add(solution6);
+		solutionList.add(solutionChild);
+		solutionList.add(solutionChild);
+		solutionList.add(solutionChild);
+		solutionList.add(solutionChild);
+		solutionList.add(solutionChild);
+		solutionList.add(solutionChild);
+		
+		return solutionList;
+	}
+	
 }
